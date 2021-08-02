a84eb1b5b345d6188fb7f2294867e91e98ebec72
ArmanMamyan <armmamyan@gmail.com>
2021-08-02 12:46:17 +0400

    Image Effect Builder Styling issues

--- src/components/effectItem/EffectItem.js
+++ src/components/effectItem/EffectItem.js
@@ -6,7 +6,7 @@ function EffectItem({filterProps, filterName}) {
     return(
         <div className='image--effect-item shadow-sm me-3 mb-3'>
             <img src={asset} alt="Default Image Effect" style={{filter: filterProps}} />
-            <span className="px-2 py-3">{filterName}</span>
+            <span className="px-2 py-1 d-block bg-white position-relative">{filterName}</span>
         </div>
     )
 }
